564d2bdbd5c730959c67e56b2bc1c9bb522d91f9
Wittaya <wittaya.b@lansingbs.com>
2019-05-12 14:43:52 +0700

    Initial Commit

---
+++ ChatApp-IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp-IOS
+//
+//  Created by TWBKG on 12/5/2562 BE.
+//  Copyright © 2562 TWBKG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
